c3285619db2bfdb634dcf72c9509d114be732047
badhon1512 <badhon1512@gmail.com>
2020-10-27 00:52:22 +0600

    home page and login page added

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body style="background-image:url('image/aiubHome.jpg');background-position: center; background-repeat: no-repeat;background-attachment: fixed;" >
+
+
+  <form action="">
+
+   <input type="text" style="margin-top:200px;margin-left: 400px;height:35px;width:350px; font-size:20px;" placeholder="username"><br>
+
+   <input type="text" style="margin-top:20px;margin-left:400px;height:35px;width:350px ; font-size:20px"placeholder="password">
+   <br>
+
+   <input type="submit" value="LogIn" style="margin-left: 400px; margin-top:20px; width:360px;height:45px; background-color:#497FAF;font-size:20px;">
+
+   
+
+
+  </form>
+
+  
+</body>
+</html>
---
+++ universityhome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body  style="background-image:url('image/home1.jpg');background-position: center; background-repeat: no-repeat;background-attachment: fixed;  ">
+  
+  <ul style="list-style-type:none; background-color:rgb(252, 90, 3); overflow:hidden; margin:0; padding: 0;">
+
+    <li style="float:right; padding:20px ; ;margin-right:100px;padding-left:70px; font-size:20px; text-align:center; display:block;"><a  href="login.php" style="color:white;  text-decoration: none;">LogIn</a></li>
+
+    <li style="float:right; padding:20px; padding-left:70px; font-size:20px;"><a href="#" style="color:white;  text-decoration: none;">Depertment</a  href="#"></li>
+    <li style="float:right; padding:20px; padding-left:70px; font-size:20px;"><a href="#" style="color:white;  text-decoration: none;">Faculty</a></li>
+    <li style="float:right; padding:20px;padding-left:70px; font-size:20px;"><a href="#" style="color:white;  text-decoration: none;">About</a></li>
+    <li style="float:right; padding:20px;padding-left:70px; font-size:20px;"><a href="#"style="color:white;  text-decoration: none;">home</a></li>
+    
+    
+  </ul>
+
+
+
+
+</body>
+</html>
